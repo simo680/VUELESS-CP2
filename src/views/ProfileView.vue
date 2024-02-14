8642890022c676cbe927b7b1d7bfe0a3aefeb751
Simo680 <erik.simonyan2005@gmail.com>
2024-02-14 09:30:36 +0300

    update commit

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -2,13 +2,14 @@
   <div>
     <div class="info">
       <div class="info__profile">
-        <span>Username: {{ this.response.username}}</span>
-        <span>Name: {{ this.response.firstname}}</span>
-        <span>LastName: {{ this.response.lastName}}</span>
-        <span>Gender: {{this.response.gender}}</span>
-        <span>Email: {{ this.response.email}}</span>
+        <h2>My profile</h2>
+        <span class="info__profile_item">Username: {{ this.response.username }}</span>
+        <span class="info__profile_item">Name: {{ this.response.firstName }}</span>
+        <span class="info__profile_item">LastName: {{ this.response.lastName }}</span>
+        <span class="info__profile_item">Gender: {{ this.response.gender }}</span>
+        <span class="info__profile_item">Email: {{ this.response.email }}</span>
       </div>
-      <img :src="this.response.image" alt="" />
+      <img class="info__profile_img" :src="this.response.image" alt="" />
     </div>
   </div>
 </template>
@@ -19,8 +20,8 @@ import axios from 'axios'
 export default {
   data() {
     return {
-       response: {},
-       error: "",
+      response: {},
+      error: ''
     }
   },
   mounted() {
@@ -39,12 +40,39 @@ export default {
         }
       })
       .catch((error) => {
-
         console.error(error)
-        this.res.message = error.response.data.message;
+        this.res.message = error.response.data.message
       })
   }
 }
 </script>
 
-<style></style>
+<style>
+.info {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 50px 50px;
+}
+
+
+.info__profile_img {
+  width: 243px;
+  height: 243px;
+  border-radius: 50%;
+}
+
+.info__profile {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+.info__profile_item {
+  padding: 30px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  text-align: center;
+  background-color: aliceblue;
+}
+</style>
